fd20cea90d20b5b487459f4dc7a087a56dfeeb8e
Enzo Joel Steier <enzo.steier@konecta.com>
2026-02-16 21:44:27 -0300

    validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // El 0.0.0.0 es la única "regla de oro" que mantenemos
+
+app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Elimina datos que no estén en el DTO
+      forbidNonWhitelisted: true, // Tira error si envían datos extra
+      transform: true, // Convierte tipos automáticamente
+    }),
+  );
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
