e9c42790d18b14450edb3c5699746dfd0c2ea03c
Enzo Joel Steier <enzo.steier@konecta.com>
2026-02-18 15:21:26 -0300

    iniciando entidades de finazas

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { FinanceModule } from "./finance/finance.module";
 // Importa aquí tus otros módulos (AuthModule, UsersModule, etc.)
 
 @Module({
